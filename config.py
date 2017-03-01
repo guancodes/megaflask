4059938e5a58d06866b9db6f269728424a1c8e1c
Guan Wang <ggwangguan@gmail.com>
2017-02-28 22:08:47 -0800

    apply patch $ specify import folder

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir 'db_repository')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
